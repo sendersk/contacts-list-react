7caa5f4cbf29b11398a7ecf6dd37e1b92a76931c
Przemyslaw Senderski <psenderski94@gmail.com>
2021-03-06 22:18:15 +0100

    Navigation component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navigation from "./components/Navigation/index"
 
 function App() {
   return (
     <div>
-      Contacts list
+      <Navigation />
     </div>
   );
 }
