31c2e2549e2af536dbe3f455021ee0724df2a94d
Przemyslaw Senderski <psenderski94@gmail.com>
2021-03-06 22:18:36 +0100

    GlobalStyle component styles changed

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,14 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    html {
+    * {
         box-sizing: border-box;
-        padding: 0;
-        margin: 0;
     }
-
-    *, ::after, ::before {
+    ::after, ::before {
         box-sizing: inherit;
+    }
+    body {
         font-family: "Montserrat", sans-serif;
+        margin: 0;
+        padding: 0;
     }
 `;
