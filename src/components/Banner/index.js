72ecb03238a26e9137497c9a8e40f0d12fabf8e2
Przemyslaw Senderski <psenderski94@gmail.com>
2021-03-06 23:14:32 +0100

    Banner component moved

---
+++ src/components/Banner/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {Wrapper} from "./styled"
+
+function Banner() {
+  return <Wrapper>Contacts</Wrapper>;
+}
+
+export default Banner;
---
+++ src/components/Banner/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 7vh;
+  align-items: center;
+  justify-content: center;
+  background-image: linear-gradient(15deg, #13547a 0%, #80d0c7 100%);
+  color: #fff;
+`;
