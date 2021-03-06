3b9d84c31f4e481cc0113896f80b50439fa571b3
Przemyslaw Senderski <psenderski94@gmail.com>
2021-03-06 23:26:43 +0100

    Item styles added

---
+++ src/components/Item/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    border-bottom: 2px solid #888;
+`
