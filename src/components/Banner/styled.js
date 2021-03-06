08be2754f7f48efa42e468c6525c22632434b778
Przemyslaw Senderski <psenderski94@gmail.com>
2021-03-07 00:31:09 +0100

    Wrapper font-size added

--- src/components/Banner/styled.js
+++ src/components/Banner/styled.js
@@ -4,6 +4,7 @@ export const Wrapper = styled.div`
   display: flex;
   width: 100%;
   height: 7vh;
+  font-size: 24px;
   align-items: center;
   justify-content: center;
   background-image: linear-gradient(15deg, #13547a 0%, #80d0c7 100%);
