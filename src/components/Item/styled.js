a9323d3a24505b49667b69792e105c80c413fb83
Przemyslaw Senderski <psenderski94@gmail.com>
2021-03-08 22:12:07 +0100

    Avatar styles modified

--- src/components/Item/styled.js
+++ src/components/Item/styled.js
@@ -1,32 +1,34 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    display: flex;
-    align-items: center;
-    width: 100vw;
-    height: 10vh;
-    border-bottom: 2px solid #aaa;
-    padding: 5px;
-`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 10vh;
+  border-bottom: 2px solid #aaa;
+  padding: 5px;
+`;
 
 export const Avatar = styled.img`
-    border-radius: 50%;
-    border: 2px solid #aaa;
-`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 2px solid #aaa;
+`;
 
 export const Content = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    padding-left: 10px;
-`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding-left: 10px;
+`;
 
 export const Header = styled.h2`
-    font-size: 20px;
-    margin: 0px;
-`
+  font-size: 20px;
+  margin: 0px;
+`;
 
 export const Paragraph = styled.p`
-    font-size: 14px;
-    margin: 0px;
-`
+  font-size: 14px;
+  margin: 0px;
+`;
