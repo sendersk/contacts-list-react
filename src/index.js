c5466274d09d6da71b1743cb369b304323982d35
Przemyslaw Senderski <psenderski94@gmail.com>
2021-03-20 16:58:14 +0100

    ThemeProvider added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {GlobalStyle} from "./GlobalStyle"
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { GlobalStyle } from "./GlobalStyle";
+import { theme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
